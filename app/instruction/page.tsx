cc8894b40b43b9cafe2cfab128ae482531c6f2b8
King Edwin <kingedwin@Kings-MacBook-Pro.local>
2025-08-15 10:25:41 +0300

    Fix JSX syntax error in instruction page - Replace leftover Grid closing tag with Box - Resolve webpack compilation error for Vercel build

--- app/instruction/page.tsx
+++ app/instruction/page.tsx
@@ -118,7 +118,7 @@ export default function InstructionPage() {
                     </Box>
                   </CardContent>
                 </Card>
-              </Grid>
+              </Box>
 
               {/* Asset Information */}
               <Box>
